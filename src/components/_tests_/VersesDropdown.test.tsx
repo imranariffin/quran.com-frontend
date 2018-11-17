fbd857804f173225f44162264528b06b10d88fca
Imran Ariffin <ariffin.imran@gmail.com>
2018-11-17 02:07:20 -0500

    add missing `chapter` prop to `VersesDropdown.test`

--- src/components/_tests_/VersesDropdown.test.tsx
+++ src/components/_tests_/VersesDropdown.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import VersesDropdown from '../VersesDropdown';
 import verse from '../../../tests/fixtures/verse';
+import { chapter } from '../../../tests/fixtures/chapters';
 
 const props = {
+  chapter,
   onClick: jest.fn(),
   isReadingMode: false,
   verses: { '1:1': verse },
